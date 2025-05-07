b56df382b3eb44e66415709f04fbf33a4395d5ca
EnriqueMacias <rain0fwolfs@gmail.com>
2025-05-07 22:53:20 +0200

    multer fixing

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -3,15 +3,19 @@ const router = express.Router();
 const { uploadProfilePicture } = require('../controllers/fileController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const { upload } = require('../services/fileService');
+const multer = require('multer');
 
 const handleMulterError = (err, req, res, next) => {
-  if (err instanceof multer.MulterError) {
-    return res.status(400).json({ error: `Error de subida: ${err.message}` });
-  } else if (err) {
-    return res.status(400).json({ error: err.message });
-  }
-  next();
-};
+    console.log('Error en multer:', err);
+    if (err instanceof multer.MulterError) {
+      // Errores específicos de multer (como límite de tamaño)
+      return res.status(400).json({ error: `Error de subida: ${err.message}` });
+    } else if (err) {
+      // Otros errores (como fileFilter)
+      return res.status(400).json({ error: err.message });
+    }
+    next();
+  };
 
 router.post('/upload-profile-picture', authMiddleware(), upload, handleMulterError, uploadProfilePicture);
 
